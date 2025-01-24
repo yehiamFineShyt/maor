1da39ba67dc04d40c82513bef5618403157aaecf
yehiamFineShyt <yehiamknafo5757@gmail.com>
2025-01-24 11:20:14 +0200

    added cluster

--- db/mongoconnect.js
+++ db/mongoconnect.js
@@ -1,12 +1,12 @@
 // 2
 const mongoose = require('mongoose');
-
+const {secretList} = require('../config/secret.js');
 main().catch(err => console.log(err));
 
 async function main() {
   // mongoose.set('strictQuery' , false);
 
-  await mongoose.connect('mongodb://127.0.0.1:27017/maor-server-js-file');
+  await mongoose.connect(`mongodb+srv://${secretList.USERNAME}:${secretList.PASSWORD}@cluster0.eyhd9.mongodb.net/`);
   console.log("mongo connect started");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
